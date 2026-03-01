01edb20ceeb6d53110b387cda441b1cb07b916b6
Javier Mombiela <61723252+javim7@users.noreply.github.com>
2026-02-28 19:43:23 -0600

    preload metadata

--- src/features/cattle/GyrLechero.jsx
+++ src/features/cattle/GyrLechero.jsx
@@ -35,7 +35,7 @@ export default function GyrLechero() {
                 id="gyr-video"
                 className="w-full h-full object-cover"
                 controls={isPlaying}
-                preload="none"
+                preload="metadata"
                 poster="/images/Ganado/ganadoPoster.jpeg"
                 title={t('videoTitle')}
                 playsInline
